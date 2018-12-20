80ff34ef8b8b48e086ed81c0d66dbc6855ae3ab6
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2018-12-20 15:06:14 +0000

    Resposta da Questão 2 do Exercício 2 de Fila

---
+++ Questão2.py
@@ -0,0 +1,26 @@
+import time
+import random 
+
+class Fila_impressao:
+  def __init__(self):
+    self.fila = []
+  
+  def requisicao(self,documento):
+    self.fila.append(documento)
+  
+  def isEmpty(self):
+    return len(self.fila) == 0
+
+  def impressao(self,numero):
+    while not(self.isEmpty()):
+      time.sleep(numero)
+      print(self.fila[0])
+      print('Impresso')
+      self.fila.pop(0)
+
+Impressora = Fila_impressao()
+Impressora.requisicao('Carlos')
+Impressora.requisicao('Isaque')
+Impressora.requisicao('Kelly Ki')
+numero = random.randint(1,10)
+Impressora.impressao(numero)
