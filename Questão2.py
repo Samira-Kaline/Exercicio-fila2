6ce660aa8b0e00ccd81ebd057079e6a896ec06c6
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2019-02-09 11:44:00 -0200

    Update Questão2.py

--- Questão2.py
+++ Questão2.py
@@ -12,18 +12,38 @@ class Fila_impressao:
     return len(self.fila) == 0
 
   def impressao(self,numero):
-    while not(self.isEmpty()):
+    Impresso = False
+    if not(self.isEmpty()):
       time.sleep(numero)
-      print(self.fila[0])
-      print('Impresso')
+      print("Documento: %s "%(self.fila[0]))
       self.fila.pop(0)
+      Impresso = True
+    return Impresso
+      
 def main():
   Impressora = Fila_impressao()
-  Impressora.requisicao('Carlos')
-  Impressora.requisicao('Isaque')
-  Impressora.requisicao('Kelly Ki')
-  numero = random.randint(1,10)
-  Impressora.impressao(numero)
+  opcao = 0
+  while(opcao!=3):
+    print("1-Requerir uma impressao")
+    print('2-Imprimir')
+    print("3-Sair")
+    opcao = int(input())
+    if(opcao==1):
+      Documento = input("Digite o nome do Documento: ")
+      Impressora.requisicao(Documento)
+      print("Documento adicionado")
+    elif(opcao==2):
+      inicio = time.time()
+      print("Imprimindo...")
+      numero = random.randint(1,10)
+      Impresso = Impressora.impressao(numero)
+      if(Impresso):
+        print("Impresso!")
+      else:
+        print("Nao tem documento, "+
+          "Por favor, Digite 1 Para Requerir uma Impressao")
+      fim = time.time()
+      print("Tempo de Impressao: %f"%(fim-inicio))
  
 if(__name__=="__main__"):
   main()
